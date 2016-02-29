7c0732cf329df09bccc202a7535a6497d47e95de
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-02-29 10:08:50 -0500

    Traditional HTML display of a list

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # php5-list-tutorial
 Tutorial on displaying data in PHP. Not intended for new developers; light PHP background and familiarity with other languages required. Uses Composer, Guzzle, and Smarty.
+
+## Step One
+```
+git checkout v1
+```
+
---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>My Great List Application</title>
+</head>
+
+<body>
+    <h1>My great list!</h1>
+    
+    <!-- This is traditional HTML without any dynamic data. Boring. -->
+    Item One<br/>
+    Item Two<br/>
+    Item Three<br/>
+    Item Four<br/>
+    Item Five
+</body>
+
+</html>
