3b57df4727372b017af916464ba3538485d8a183
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-02-29 10:17:53 -0500

    Document last code

--- index.php
+++ index.php
@@ -10,10 +10,12 @@
     <?php
     $filename = "list.json";
 
+    # Currently this file does not exist so an error will be displayed.
     if (!file_exists($filename)) {
         echo "File ({$filename}) does not exist!";
     } else {
         $contents = file_get_contents($filename);
+        # If the file existed we would display the contents.
         echo $contents;
     }
     ?>
